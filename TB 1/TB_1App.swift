5aeb5495eb3b95b6998b0b47651979c510300094
AQZHAR <aqctivity@gmail.com>
2024-11-02 22:20:17 +0700

    Initial Commit

---
+++ TB 1/TB_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TB_1App.swift
+//  TB 1
+//
+//  Created by Aqsal Ashari on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TB_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
